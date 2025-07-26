012d459b8565047c2f7baa0aded6993097f6bee9
Sagar Kumar <117724228+Unknownmaster0@users.noreply.github.com>
2025-07-26 15:34:18 +0530

    Fix MongoDB connection and environment setup

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -7,9 +7,6 @@ type connectionObject = {
 
 const connection: connectionObject = {};
 
-const DB_URI = "mongodb+srv://trySingh:sa123@try-clustor0.59gvu.mongodb.net/";
-const DB_NAME = "Feedback-app";
-
 // MAIN CONNECTION LOGIC OF THE DABATASE.
 const connectDb = async () => {
   // IF ALREADY HAVE THE CONNECTION WITH DB, THEN NO NEED TO MAKE THE DB CONNECTION AGAIN.
@@ -19,11 +16,8 @@ const connectDb = async () => {
   }
   try {
     const connectionResponse = await mongoose.connect(
-      `${DB_URI}/${DB_NAME}`
+      process.env.MONGO_URI!
     );
-    // const connectionResponse = await mongoose.connect(
-    //   `${process.env.DB_URI}/${process.env.DB_NAME}`
-    // );
     connection.isConnected = connectionResponse.connections[0].readyState;
   } catch (error) {
     log("error while connection with db", error);
